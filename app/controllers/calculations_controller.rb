350e1dd43df1da477c3e8c177550a75a2f83589c
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-10-30 17:47:53 -0500

    the square works! :D

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,8 +1,9 @@
-class CalculationController < ApplicationController
+class CalculationsController < ApplicationController
   def home
+  	@siggn = params["number"].to_i
   end
 
-  def square_root
+  def sqrt
     @the_number = params[:number].to_f
     @answer = Math.sqrt(@the_number)
   end
